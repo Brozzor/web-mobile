0085d0a0455af630aa5dc057430fd2da3d68e220
Bromain <377roro@gmail.com>
2022-10-14 15:58:12 +0200

    mise en place de leaflet

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,100 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="navbar">
+
+</div>
+  <div>
+    <div>
+      <span v-if="loading">Loading...</span>
+      <label for="checkbox">GeoJSON Visibility</label>
+      <input
+        id="checkbox"
+        v-model="show"
+        type="checkbox"
+      >
+      <label for="checkboxTooltip">Enable tooltip</label>
+      <input
+        id="checkboxTooltip"
+        v-model="enableTooltip"
+        type="checkbox"
+      >
+      <input
+        v-model="fillColor"
+        type="color"
+      >
+      <br>
+    </div>
+    <l-map
+      :zoom="zoom"
+      :center="center"
+      style="height: 500px; width: 100%"
+    >
+      <l-tile-layer
+        :url="url"
+        :attribution="attribution"
+      />
+      <l-geo-json
+        v-if="show"
+        :geojson="geojson"
+        :options-style="styleFunction"
+      />
+      <l-marker :lat-lng="marker" />
+    </l-map>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// DON'T load Leaflet components here!
+// Its CSS is needed though, if not imported elsewhere in your application.
+import "leaflet/dist/leaflet.css"
+import { latLng } from "leaflet";
+import { LMap, LTileLayer, LMarker, LGeoJson } from "@vue-leaflet/vue-leaflet";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    LMap,
+    LTileLayer,
+    LGeoJson,
+    LMarker
+  },
+  data() {
+    return {
+      loading: false,
+      show: true,
+      enableTooltip: true,
+      zoom: 8,
+      center: [48, -1.219482],
+      geojson: null,
+      fillColor: "#e4ce7f",
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      marker: latLng(47.41322, -1.219482)
+    };
+  },
+  computed: {
+    styleFunction() {
+      const fillColor = this.fillColor;
+      return () => {
+        return {
+          weight: 2,
+          color: "#ECEFF1",
+          opacity: 1,
+          fillColor: fillColor,
+          fillOpacity: 1
+        };
+      };
+    }
+  },
+  async created() {
+    this.loading = true;
+    const response = await fetch("https://rawgit.com/gregoiredavid/france-geojson/master/regions/pays-de-la-loire/communes-pays-de-la-loire.geojson")
+    const data = await response.json();
+    this.geojson = data;
+    this.loading = false;
   }
-}
+};
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
